662d236d53fcdc130d750968316929d957b893fc
Frederick Corcino Alejo <fcorcino@intellisys.com.do>
2018-03-29 13:07:28 -0400

    Initial commit

---
+++ app/src/main/java/com/icode/rentcar/home/HomeActivity.kt
@@ -0,0 +1,55 @@
+package com.icode.rentcar.home
+
+import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import com.icode.rentcar.R
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.toolbar.*
+
+class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+        )
+
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_add_car -> null // Display `AddCarFragment`
+            R.id.nav_track_cars -> null // Display `TrackCarsFragment`
+            R.id.nav_rent_car -> null // Display `RentCarFragment`
+            R.id.nav_find_car_lot -> null // Display `FindCarLotFragment`
+            R.id.nav_all_business -> null // Display `BusinessFragment`
+            R.id.nav_my_business -> null // Display `BusinessFragment` filtered by user
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
